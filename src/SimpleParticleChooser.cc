fef404435eb777947d790bb416ba481bdb43de72
john.shumwayjr <john.shumwayjr@6df8c730-e442-11dd-8fb1-9960f2a117f8>
2009-01-21 18:37:23 +0000

    Fixed a rare-event bug in SimpleParticleChooser.cc
    git-svn-id: https://pi-qmc.googlecode.com/svn/trunk@8 6df8c730-e442-11dd-8fb1-9960f2a117f8

--- src/SimpleParticleChooser.cc
+++ src/SimpleParticleChooser.cc
@@ -33,6 +33,7 @@ SimpleParticleChooser::~SimpleParticleChooser() {}
 void SimpleParticleChooser::chooseParticles() {
   for (int imoving=0; imoving<index.size();) {
     int i = (int)(npart*RandomNumGenerator::getRand());
+    if (i==npart) continue;
     for (int jmoving=0; jmoving<=imoving; ++jmoving) {
       if (jmoving==imoving) index(imoving++)=i;
       if (index(jmoving)==i) break;
