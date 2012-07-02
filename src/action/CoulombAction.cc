09b14eaaa83d977b43fbe804ffbae26c56ec4925
John Shumway <john.shumwayjr@gmail.com>
2012-07-01 22:13:47 -0700

    Cleaned up 3D coulomb code; ready to refactor.

--- src/action/CoulombAction.cc
+++ src/action/CoulombAction.cc
@@ -199,112 +199,47 @@ double CoulombAction::getEField(const Paths& paths, int ipart,
   return fp[0]/tau;
 }
 
-
-
 double CoulombAction::u(double r, int order) const {
-	double u = 0;
 	r = sqrt(r * r + displace2);
 	double taueff = 2.0 * mu * q1q2 * q1q2 * tau;
 	double reff = 2.0 * mu * q1q2 * r;
 	double stau = q1q2 * sqrt(2.0 * mu * tau);
-	//First compute the 1D actions.
+	// First compute the 1D actions.
 	Coulomb1DLinkAction coulomb1D(stau);
 	double u0 = coulomb1D.calculateValueAtOrigin();
     double u1_0 = coulomb1D.calculateU0(u0, reff, taueff);
-	double u1_1 = 0., u1_2 = 0., u1_3 = 0., u1_4 = 0.;
-    {
-        // u1 for 1D
-        double a = 0.07776906 * pow(stau, -1);
-        double b = -0.04695226 * pow(stau, -2);
-        double c = 0.01291994 * pow(stau, -3);
-        double d = 0.60942687 * pow(stau, -1);
-        double e = -1.09588064 * pow(stau, -2);
-        double f = 1.15591565 * pow(stau, -3);
-        double g = -0.57485278 * pow(stau, -4);
-        double h = 0.15212765 * pow(stau, -5);
-        u1_1 = pow(reff, 2) * (a + reff * (b + reff * c)) / (1 + reff * (d + reff * (e + reff * (f + reff * (g + reff * h)))));
-    }
-    {
-        // u2 for 1D
-        double a = 9.52130652e-04 * pow(stau, -3);
-        double b = -6.39946071e-04 * pow(stau, -4);
-        double c = 1.14359558e-04 * pow(stau, -5);
-        double d = -7.59575215e-01 * pow(stau, -3);
-        double e = 5.53798980e-01 * pow(stau, -4);
-        double f = -7.15626226e-02 * pow(stau, -5);
-        double g = -3.49081618e-02 * pow(stau, -6);
-        double h = 8.40084881e-03 * pow(stau, -7);
-        u1_2 = pow(reff, 4) * (a + reff * (b + reff * c)) / (1 + pow(reff, 3) * (d + reff * (e + reff * (f + reff * (g + reff * h)))));
-    }
-    {
-        // u3 for 1D
-        double a = -2.06984256e-1 * pow(stau, -5);
-        double b = -9.51423947e-3 * pow(stau, -6);
-        double c = 8.97532561e-2 * pow(stau, -7);
-        double d = 6.10876797e+4 * pow(stau, -3);
-        double e = -1.19016292e+5 * pow(stau, -4);
-        double f = 9.29836599e+4 * pow(stau, -5);
-        double g = -3.49919130e+4 * pow(stau, -6);
-        double h = 6.42490539e+3 * pow(stau, -7);
-        double i = -6.02729064e+2 * pow(stau, -8);
-        double j = 6.01285550e+1 * pow(stau, -9);
-        u1_3 = pow(reff, 6) * (a + reff * (b + reff * c)) / (1 + pow(reff, 3) * (d + reff * (e + reff * (f + reff * (g + reff * (h + reff * (i + reff * j)))))));
-    }
-    {
-        // u4 for 1D
-        double a = -2.84102296e-6 * pow(stau, -5);
-        double b = 6.26961672e-7 * pow(stau, -7);
-        double c = -2.20166473e-1 * pow(stau, -3);
-        double d = 1.07981903e-1 * pow(stau, -5);
-        double e = -1.94671111e-2 * pow(stau, -7);
-        double f = 1.56217930e-3 * pow(stau, -9);
-        u1_4 = pow(reff, 6) * (a + b * reff * reff) / (1 + reff * reff * reff * (c + reff * reff * (d + reff * reff * (e + reff * reff * f))));
-    }
+	double u1_1 = coulomb1D.calculateU1(stau, reff);
+	double u1_2 = coulomb1D.calculateU2(stau, reff);
+	double u1_3 = coulomb1D.calculateU3(stau, reff);
+	double u1_4 = coulomb1D.calculateU4(stau, reff);
+
+	// Then extract the 3D actions.
     double a = 2. * taueff / (reff * reff);
     double b = exp(-2. / a);
     double c = 1. / (1 + 2 * a * (1 - b) * u1_1);
-    // Equations from Mathematica's power series expansion.
-    if(order == 0){
+
+	double u = 0;
+    switch (order) {
+    case 0:
         u = u1_0 + log(c);
-        if(ewaldSum){
-            u -= tau * q1q2 * ewaldSum->evalFR(r);
-        }
+    	break;
+    case 1:
+    	u = u1_1 + c * (b * u1_1 - 4 * a * (1 - b) * u1_2);
+    	break;
+    case 2:
+    	u = u1_2 + 0.25 * c / a * (c * (b * u1_1 * (1 + 2 * a * u1_1) + 8 * a * b * u1_2 + 32 * a * a * a * (1 - b) * (1 - b) * u1_2 * u1_2) - 24 * a * a * (1 - b) * u1_3);
+    	break;
+    case 3:
+    	u = u1_3 + c * c * c / (24. * a * a) * (128 * a * a * a * a * a * b * b * b * (4 * u1_2 * u1_2 * u1_2 - 9 * u1_1 * u1_2 * u1_3 + 6 * u1_1 * u1_1 * u1_4) + 64 * a * a * a * (a * u1_2 * (-8 * a * u1_2 * u1_2 + 9 * (1 + 2 * a * u1_1) * u1_3) - 3 * (1 + 2 * a * u1_1) * (1 + 2 * a * u1_1) * u1_4) + 2 * a * b * b * (2 * a * u1_1 * u1_1 * u1_1 + 96 * a * a * u1_2 * (u1_2 - 8 * a * a * u1_2 * u1_2 + 3 * a * u1_3) + 12 * a * u1_1 * (u1_2 - 6 * a * u1_3 + 144 * a * a * a * u1_2 * u1_3 - 32 * a * a * u1_4) + u1_1 * u1_1 * (1 - 1152 * a * a * a * a * u1_4)) + b * (4 * a * a * u1_1 * u1_1 * u1_1 + 12 * a * (u1_2 - 16 * a * a * u1_2 * u1_2 + 128 * a * a * a * a * u1_2 * u1_2 * u1_2 + 6 * a * u1_3 - 96 * a * a * a * u1_2 * u1_3 + 16 * a * a * u1_4) + 4 * u1_1 * u1_1 * (a + 576 * a * a * a * a * a * u1_4) + u1_1 * (1 + 24 * a * a * (u1_2 + 6 * a * u1_3 - 144 * a * a * a * u1_2 * u1_3 + 64 * a * a * u1_4))));
+    	break;
+    case 4:
+    	u = u1_4; //Not exact
+    	break;
     }
-    else
-        if(order == 1){
-            u = u1_1 + c * (b * u1_1 - 4 * a * (1 - b) * u1_2);
-        }else
-            if(order == 2){
-                u = u1_2 + 0.25 * c / a * (c * (b * u1_1 * (1 + 2 * a * u1_1) + 8 * a * b * u1_2 + 32 * a * a * a * (1 - b) * (1 - b) * u1_2 * u1_2) - 24 * a * a * (1 - b) * u1_3);
-            }else
-                if(order == 3){
-                    u = u1_3 + c * c * c / (24. * a * a) * (128 * a * a * a * a * a * b * b * b * (4 * u1_2 * u1_2 * u1_2 - 9 * u1_1 * u1_2 * u1_3 + 6 * u1_1 * u1_1 * u1_4) + 64 * a * a * a * (a * u1_2 * (-8 * a * u1_2 * u1_2 + 9 * (1 + 2 * a * u1_1) * u1_3) - 3 * (1 + 2 * a * u1_1) * (1 + 2 * a * u1_1) * u1_4) + 2 * a * b * b * (2 * a * u1_1 * u1_1 * u1_1 + 96 * a * a * u1_2 * (u1_2 - 8 * a * a * u1_2 * u1_2 + 3 * a * u1_3) + 12 * a * u1_1 * (u1_2 - 6 * a * u1_3 + 144 * a * a * a * u1_2 * u1_3 - 32 * a * a * u1_4) + u1_1 * u1_1 * (1 - 1152 * a * a * a * a * u1_4)) + b * (4 * a * a * u1_1 * u1_1 * u1_1 + 12 * a * (u1_2 - 16 * a * a * u1_2 * u1_2 + 128 * a * a * a * a * u1_2 * u1_2 * u1_2 + 6 * a * u1_3 - 96 * a * a * a * u1_2 * u1_3 + 16 * a * a * u1_4) + 4 * u1_1 * u1_1 * (a + 576 * a * a * a * a * a * u1_4) + u1_1 * (1 + 24 * a * a * (u1_2 + 6 * a * u1_3 - 144 * a * a * a * u1_2 * u1_3 + 64 * a * a * u1_4))));
-                }else
-                    if(order == 4){
-                        u = u1_4; //Not exact
-                    }
 
-
-
-
-    //double ureff = 2.0*mu*fabs(q1q2)*r;
-    //double sign= (q1q2>0)?1:-1;
-    //double reff = 2.0*mu*q1q2*r;
-    //double stau = q1q2*sqrt(2.0*mu*tau);
-    //double u0 = stau*(2.784163998+stau*(-0.331414576+stau*(0.069113708
-    //           +stau*(-0.007701621+stau*(-0.000062512+stau*0.000335724)))));
-    //double a = 0.81644994/stau + 0.20750388;
-    //double b = 0.73884476/taueff + 0.22874030/stau;
-    //double c = 0.48135991*pow(stau,-3) + 0.15828689*pow(stau,-2);
-    //double d = 0.15769661*pow(stau,-4) + 0.01512610*pow(stau,-3);
-    //double e = 0.25472475*pow(stau,-5) + 0.16019595*pow(stau,-4);
-    //double f = 0.51100692/stau + 0.15237602;
-    //double g = 0.15705841/taueff + 0.01503638/stau;
-    //u = (u0 + reff*( (-2.+u0*a) + reff*(f + reff*(g + e*taueff*reff)))) /
-    //    (1. + reff*(a + reff*(b + reff*(c + reff*(d + e*reff)))));
-    //ureff=2.0*mu*fabs(q1q2)*sqrt(r*r+screenDist*screenDist);
-    //u -= (u0 + ureff*( (-2.+u0*a) + ureff*(f + ureff*(g + e*taueff*ureff)))) /
-    //     (1. + ureff*(a + ureff*(b + ureff*(c + ureff*(d + e*ureff)))));
+    if(order == 0 && ewaldSum){
+            u -= tau * q1q2 * ewaldSum->evalFR(r);
+    }
 
     return u;
 }
--- src/action/coulomb/Coulomb1DLinkAction.cpp
+++ src/action/coulomb/Coulomb1DLinkAction.cpp
@@ -1,4 +1,5 @@
 #include "Coulomb1DLinkAction.h"
+#include <cmath>
 
 Coulomb1DLinkAction::Coulomb1DLinkAction(double stau) :
 		stau(stau),
@@ -23,7 +24,7 @@ double Coulomb1DLinkAction::calculateValueAtOrigin() const {
 	return u0;
 }
 
-double Coulomb1DLinkAction::calculateU0(double u0, double reff,
+double Coulomb1DLinkAction::calculateU0(double uOrigin, double reff,
 		double taueff) const {
 
 	double a = 0.25300593 * stauToMinus1 + 0.01432126;
@@ -34,11 +35,103 @@ double Coulomb1DLinkAction::calculateU0(double u0, double reff,
 	double f = 0.18976335 * stauToMinus1;
 	double g = 0.00343053 * stauToMinus2;
 
-	double u1_0 = (u0
-			+ reff
-					* ((u0 * a - 1.)
+	double u0 = (uOrigin + reff * ((u0 * a - 1.)
 							+ reff * (f + reff * (g + reff * e * taueff))));
-	u1_0 /= (1.
+	u0 /= (1.
 			+ reff * (a + reff * (b + reff * (c + reff * (d + reff * e)))));
-	return u1_0;
+	return u0;
 }
+
+double Coulomb1DLinkAction::calculateU1(double stau, double reff) const {
+	double a = 0.07776906 * stauToMinus1;
+	double b = -0.04695226 * stauToMinus2;
+	double c = 0.01291994 * stauToMinus3;
+	double d = 0.60942687 * stauToMinus1;
+	double e = -1.09588064 * stauToMinus2;
+	double f = 1.15591565 * stauToMinus3;
+	double g = -0.57485278 * stauToMinus4;
+	double h = 0.15212765 * stauToMinus5;
+	double u1 = reff * reff * (a + reff * (b + reff * c));
+	u1 /= (1 + reff * (d + reff * (e + reff * (f + reff * (g + reff * h)))));
+	return u1;
+}
+
+double Coulomb1DLinkAction::calculateU2(double stau, double reff) const {
+	double a = 9.52130652e-04 * pow(stau, -3);
+	double b = -6.39946071e-04 * pow(stau, -4);
+	double c = 1.14359558e-04 * pow(stau, -5);
+	double d = -7.59575215e-01 * pow(stau, -3);
+	double e = 5.53798980e-01 * pow(stau, -4);
+	double f = -7.15626226e-02 * pow(stau, -5);
+	double g = -3.49081618e-02 * pow(stau, -6);
+	double h = 8.40084881e-03 * pow(stau, -7);
+	double u1_2 =
+			pow(reff, 4) * (a + reff * (b + reff * c))
+			/ (1
+					+ pow(reff, 3)
+					* (d
+							+ reff
+							* (e
+									+ reff
+									* (f
+											+ reff
+											* (g
+													+ reff
+													* h)))));
+	return u1_2;
+}
+
+double Coulomb1DLinkAction::calculateU3(double stau, double reff) const {
+	double a = -2.06984256e-1 * pow(stau, -5);
+	double b = -9.51423947e-3 * pow(stau, -6);
+	double c = 8.97532561e-2 * pow(stau, -7);
+	double d = 6.10876797e+4 * pow(stau, -3);
+	double e = -1.19016292e+5 * pow(stau, -4);
+	double f = 9.29836599e+4 * pow(stau, -5);
+	double g = -3.49919130e+4 * pow(stau, -6);
+	double h = 6.42490539e+3 * pow(stau, -7);
+	double i = -6.02729064e+2 * pow(stau, -8);
+	double j = 6.01285550e+1 * pow(stau, -9);
+	double u1_3 =
+			pow(reff, 6) * (a + reff * (b + reff * c))
+			/ (1
+					+ pow(reff, 3)
+					* (d
+							+ reff
+							* (e
+									+ reff
+									* (f
+											+ reff
+											* (g
+													+ reff
+													* (h
+															+ reff
+															* (i
+																	+ reff
+																	* j)))))));
+	return u1_3;
+}
+
+double Coulomb1DLinkAction::calculateU4(double stau, double reff) const {
+	double a = -2.84102296e-6 * pow(stau, -5);
+	double b = 6.26961672e-7 * pow(stau, -7);
+	double c = -2.20166473e-1 * pow(stau, -3);
+	double d = 1.07981903e-1 * pow(stau, -5);
+	double e = -1.94671111e-2 * pow(stau, -7);
+	double f = 1.56217930e-3 * pow(stau, -9);
+	double u1_4 =
+			pow(reff, 6) * (a + b * reff * reff)
+			/ (1
+					+ reff * reff * reff
+					* (c
+							+ reff * reff
+							* (d
+									+ reff * reff
+									* (e
+											+ reff
+											* reff
+											* f))));
+	return u1_4;
+}
+
+
