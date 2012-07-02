b4ed29b3266c7d4110453f08bb3d074f89568ed9
John Shumway <john.shumwayjr@gmail.com>
2012-07-01 17:14:42 -0700

    Parameterized Coulomb1DLinkActionTest.

---
+++ unit-tests/action/coulomb/Coulomb1DLinkActionTest.cc
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+#include <utility>
+
+#include "action/coulomb/Coulomb1DLinkAction.h"
+
+namespace {
+
+class Coulomb1DLinkActionTest: public ::testing::Test,
+		public ::testing::WithParamInterface<std::pair<double, double> > {
+protected:
+
+	virtual void SetUp() {
+	}
+
+	virtual void TearDown() {
+	}
+
+};
+
+TEST_P(Coulomb1DLinkActionTest, testValueAtOrigin) {
+	double stau = GetParam().first;
+	double expect = GetParam().second;
+	double value = Coulomb1DLinkAction::calculate1DValueAtOrigin(stau);
+	ASSERT_DOUBLE_EQ(expect, value)
+			<< "(tau = " << stau << ")";
+}
+
+INSTANTIATE_TEST_CASE_P(TimestepScan, Coulomb1DLinkActionTest,
+		::testing::Values(
+				std::pair<double, double> (0.0, 0.0),
+				std::pair<double, double> (0.01, 0.017717130566979017),
+				std::pair<double, double> (0.1, 0.17650980432591801),
+				std::pair<double, double> (-0.01, -0.017731958122632733),
+				std::pair<double, double> (-0.1, -0.17799263565884202)));
+
+}
