314d416e1cfa3f3e2df3dfdd11ce7209cbbe564e
John Shumway <john.shumwayjr@gmail.com>
2012-07-08 14:33:15 -0500

    Added tau derivative unit test for CoulombLinkAction.

--- unit-tests/action/coulomb/CoulombLinkActionTest.cpp
+++ unit-tests/action/coulomb/CoulombLinkActionTest.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include "action/coulomb/CoulombLinkAction.h"
+#include <blitz/tinyvec-et.h>
 
 namespace {
 
@@ -11,6 +12,7 @@ protected:
 	double epsilon;
 	double deltaTau;
 	double mu;
+	static const double PI;
 
 	virtual void SetUp() {
 		q1q2 = -1.0;
@@ -24,6 +26,32 @@ protected:
 		delete action;
 	}
 
+	double calculateUDot(CoulombLinkAction::Vec delta1,
+	        CoulombLinkAction::Vec delta2) const {
+	    double delta = 1e-6;
+        double value = calculatePropagator(deltaTau + delta, delta1, delta2);
+        value -= calculatePropagator(deltaTau - delta, delta1, delta2);
+        return value / (2.0 * delta);
+	}
+
+	double calculatePropagator(double someDeltaTau,
+	        CoulombLinkAction::Vec delta1,
+	        CoulombLinkAction::Vec delta2) const {
+        CoulombLinkAction someAction(q1q2, epsilon, mu, someDeltaTau, 3);
+        double propagator =
+                calculateFreePropagator(someDeltaTau, delta1, delta2);
+        propagator *= exp(-someAction.getValue(delta1, delta2));
+        return propagator;
+	}
+
+	double calculateFreePropagator(double someDeltaTau,
+            CoulombLinkAction::Vec delta1,
+            CoulombLinkAction::Vec delta2) const {
+	    double r2 = blitz::dot(delta1 - delta2, delta1 - delta2);
+	    double propagator = exp(-0.25 * r2 / someDeltaTau);
+	    propagator *= pow(4 * PI * someDeltaTau, -0.5 * NDIM);
+	    return propagator;
+	}
 };
 
 TEST_F(CoulombLinkActionTest, testAverageSeparation) {
@@ -35,6 +63,8 @@ TEST_F(CoulombLinkActionTest, testAverageSeparation) {
 			CoulombLinkAction::calculateAverageSeparation(delta1, delta2);
 	ASSERT_DOUBLE_EQ(4.0, value);
 }
+
+const double CoulombLinkActionTest::PI = 3.141592653389793;
 
 TEST_F(CoulombLinkActionTest, testS2) {
 	CoulombLinkAction::Vec delta1 = 0.0;
@@ -62,6 +92,17 @@ TEST_F(CoulombLinkActionTest, testDiagonalValue) {
 	double value = action->getValue(delta1, delta2);
     double expect = -0.0016612138747140238;
 	ASSERT_DOUBLE_EQ(expect, value);
+}
+
+TEST_F(CoulombLinkActionTest, testTauDerivatve) {
+    double dist = 5.0;
+    CoulombLinkAction::Vec delta1 = 0.0;
+    delta1(0) = dist;
+    CoulombLinkAction::Vec delta2 = 0.0;
+    delta2(0) = dist;
+    double value = calculateUDot(delta1, delta2);
+    double expect = -10.654095411954945;
+    ASSERT_DOUBLE_EQ(expect, value);
 }
 
 }
