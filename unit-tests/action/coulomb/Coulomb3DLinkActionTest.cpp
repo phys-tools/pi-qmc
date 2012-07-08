502fc13159ab3f7bcc9011051f035e4e56c2fc5b
John Shumway <john.shumwayjr@gmail.com>
2012-07-08 13:35:18 -0500

    Added unit tests for 3DCoulombAction.

--- unit-tests/action/coulomb/Coulomb3DLinkActionTest.cpp
+++ unit-tests/action/coulomb/Coulomb3DLinkActionTest.cpp
@@ -33,5 +33,40 @@ TEST_F(Coulomb3DLinkActionTest, testValueAtOrigin) {
 	ASSERT_DOUBLE_EQ(expect, value);
 }
 
+TEST_F(Coulomb3DLinkActionTest, testU0) {
+    double reff = 1.0;
+    double value = coulomb3D->calculateU0(reff);
+    double expect = 9.9843305484870431e-5;
+    ASSERT_DOUBLE_EQ(expect, value);
+}
+
+TEST_F(Coulomb3DLinkActionTest, testU1) {
+    double reff = 1.0;
+    double value = coulomb3D->calculateU1(reff);
+    double expect = 8.5031825295702207e-6;
+    ASSERT_DOUBLE_EQ(expect, value);
+}
+
+TEST_F(Coulomb3DLinkActionTest, testU2) {
+    double reff = 1.0;
+    double value = coulomb3D->calculateU2(reff);
+    double expect = 1.3429133944492796e-6;
+    ASSERT_DOUBLE_EQ(expect, value);
+}
+
+TEST_F(Coulomb3DLinkActionTest, testU3) {
+    double reff = 1.0;
+    double value = coulomb3D->calculateU3(reff);
+    double expect = 1.637776980179888e-7;
+    ASSERT_DOUBLE_EQ(expect, value);
+}
+
+TEST_F(Coulomb3DLinkActionTest, testU4) {
+    double reff = 1.0;
+    double value = coulomb3D->calculateU4(reff);
+    double expect = 4.0165622775654946e-8;
+    ASSERT_DOUBLE_EQ(expect, value);
+}
+
 }
 
