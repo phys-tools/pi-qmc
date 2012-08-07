72e09bad846c55f79fa4118064cdfef9d2bf1bba
John Shumway <john.shumwayjr@gmail.com>
2012-08-07 07:25:48 -0700

    Added two function files.

---
+++ src/util/ipow.h
@@ -0,0 +1,18 @@
+#ifndef IPOW_H_
+#define IPOW_H_
+
+int ipow(int base, int exp) {
+	if (exp < 0) return 0;
+    int result = 1;
+    while (exp)
+    {
+        if (exp & 1)
+            result *= base;
+        exp >>= 1;
+        base *= base;
+    }
+
+    return result;
+}
+
+#endif
