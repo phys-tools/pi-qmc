31bdbf294651dd3ec3505741647b0f7b6a4c3a0f
John Shumway <john.shumwayjr@gmail.com>
2012-12-07 15:59:16 -0700

    Implemented FFT1D using FFTW3.

--- src/util/fft/FFT1D.h
+++ src/util/fft/FFT1D.h
@@ -1,10 +1,22 @@
 #ifndef FFT1D_H_
 #define FFT1D_H_
 
+#include <complex>
+#include <fftw3.h>
+
 class FFT1D {
 public:
-    FFT1D();
+    typedef std::complex<double> Complex;
+    FFT1D(Complex *data, int dataCount);
     virtual ~FFT1D();
+
+    void forward();
+    void reverse();
+private:
+    Complex *data;
+    const int dataCount;
+    fftw_plan forwardPlan;
+    fftw_plan reversePlan;
 };
 
 #endif
