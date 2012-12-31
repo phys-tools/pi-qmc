a9b24a3b0c316533a4319ee716ec14256627ce01
John Shumway <john.shumwayjr@gmail.com>
2012-12-30 22:02:39 -0700

    Explicitly specified html_style in sphinx conf.py.

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -93,6 +93,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'f6'
+html_style = 'f6.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
