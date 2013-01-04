195b9d9e598c0c412b2453e43cd7d954a7412112
John Shumway <john.shumwayjr@gmail.com>
2013-01-04 16:45:24 -0700

    Set grid parameters to reasonable starting values.

--- src/util/propagator/GridParameters.h
+++ src/util/propagator/GridParameters.h
@@ -4,12 +4,16 @@
 class GridParameters {
 public:
     GridParameters();
+    GridParameters(double mass, double tau, double x0, double deltaX);
     virtual ~GridParameters();
 
     int getGridCount() const;
     double getDeltaX() const;
     double getXMin() const;
     int getIndex0() const;
+
+    static double calculateThermalWidth(double mass, double tau);
+    static int powerTwoCeiling(double width);
 private:
     int gridCount;
     double deltaX;
